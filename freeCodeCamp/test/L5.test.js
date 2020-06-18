6436fd3fb796dd4023265c2b25e1a829673f5479
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-18 12:13:13 -0500

    10.1

--- freeCodeCamp/test/L5.test.js
+++ freeCodeCamp/test/L5.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('tests', () => {
@@ -10,6 +10,6 @@ describe('tests', () => {
   it('should pass', async () => {
     assert(lastCommand[0] === 'echo' && lastCommand[1] === 'hello' && lastCommand[2] === 'terminal');
   });
-});
+});*/
 
 // solution
