983afe269a08a53fc21f6e20bf81fc809fdbf0a5
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-22 12:50:17 -0500

    110.1

---
+++ freeCodeCamp/test/L110.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    const correctArgument = lastCommand[1] === 'package.json' || lastCommand[1] === './package.json';
+    assert(lastCommand[0] === 'more' && correctArgument && cwd === '/home/strove/project/freeCodeCamp');
+  });
+});
