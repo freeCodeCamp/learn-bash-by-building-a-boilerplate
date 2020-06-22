fb7ddfdbc36111d449d826a340196dcd98f5fb97
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-22 17:29:59 -0500

    250.1

---
+++ freeCodeCamp/test/L250.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getPreviousCwd } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, previousCwd;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+    previousCwd = await getPreviousCwd();
+  });
+
+  it('should pass', async () => {
+    assert(lastCommand[0] === 'cd' && cwd === '/home/strove/project' && previousCwd === '/home/strove/project/freeCodeCamp/node_modules');
+  });
+});
