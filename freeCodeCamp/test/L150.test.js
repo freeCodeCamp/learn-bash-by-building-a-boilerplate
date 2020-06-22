bf814ef2ad39bdadaf556165ac6ebaf889ac83bd
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-22 17:16:43 -0500

    150.1

--- freeCodeCamp/test/L150.test.js
+++ freeCodeCamp/test/L150.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('tests', () => {
@@ -9,8 +9,6 @@ describe('tests', () => {
   });
 
   it('should pass', async () => {
-    assert(lastCommand[0] === 'cd' && cwd === '/home/strove/project/freeCodeCamp/node_modules');
+    assert(lastCommand[0] === 'ls' && cwd === '/home/strove/project/freeCodeCamp/node_modules');
   });
-});*/
-
-// solution
+});
