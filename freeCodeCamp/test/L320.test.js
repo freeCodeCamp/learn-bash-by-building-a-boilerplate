553d43213b165248b549d1f916d227d749ad9ac1
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 12:12:15 -0500

    320.1S

--- freeCodeCamp/test/L320.test.js
+++ freeCodeCamp/test/L320.test.js
@@ -13,3 +13,5 @@ describe('tests', () => {
     assert(websiteDirectory.indexOf('index.html') >= 0 && lastCommand[0] === 'touch' && /index\.html$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
