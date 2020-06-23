3b1dc97669725b0df29fec414f4e9a4b43eeb092
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 12:11:59 -0500

    320.1

--- freeCodeCamp/test/L310.test.js
+++ freeCodeCamp/test/L310.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('tests', () => {
@@ -11,6 +11,6 @@ describe('tests', () => {
   it('should pass', async () => {
     assert(lastCommand[0] === 'echo' && lastCommand[1] === 'hello' && lastCommand[2] === 'website' && cwd === '/home/strove/project/website');
   });
-});
+});*/
 
 // solution
