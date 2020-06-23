8b8bc013be61e5899909317587b216f5efc1e0fe
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 12:11:59 -0500

    320.1

---
+++ freeCodeCamp/test/L320.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, websiteDirectory;
+  before(async () => {
+    websiteDirectory = await getDirectoryContents('../website');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    assert(websiteDirectory.indexOf('index.html') >= 0 && lastCommand[0] === 'touch' && /index\.html$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
+  });
+});
