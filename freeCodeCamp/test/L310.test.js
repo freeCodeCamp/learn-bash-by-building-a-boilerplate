e9aa8da9719bd0a345455a3fecc30b495863fb0c
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 12:10:26 -0500

    310.1

--- freeCodeCamp/test/L310.test.js
+++ freeCodeCamp/test/L310.test.js
@@ -1,16 +1,14 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('tests', () => {
-  let cwd, lastCommand;
+  let lastCommand;
   before(async () => {
     lastCommand = await getLastCommand();
     cwd = await getCwd();
   });
 
   it('should pass', async () => {
-    assert(lastCommand[0] === 'ls' && cwd === '/home/strove/project/website');
+    assert(lastCommand[0] === 'echo' && lastCommand[1] === 'hello' && lastCommand[2] === 'website' && cwd === '/home/strove/project/website');
   });
-});*/
-
-// solution
+});
