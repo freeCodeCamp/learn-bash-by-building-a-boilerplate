562209a60a5703cb27141f432d1cca3156291d33
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 22:01:53 -0500

    620.1

--- freeCodeCamp/test/620.test.js
+++ freeCodeCamp/test/620.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -6,6 +6,6 @@ describe('tests', () => {
     const imagesDirectory = await getDirectoryContents('../website/images');
     const lastCommand = await getLastCommand();
 
-    assert(imagesDirectory.indexOf('header.png') >= 0 && lastCommand[0] === 'cp' && /header\.png$/.test(lastCommand[1]) && /images/.test(lastCommand[2]));
+    assert(imagesDirectory.indexOf('footer.jpeg') >= 0 && lastCommand[0] === 'cp' && /footer\.jpeg$/.test(lastCommand[1]) && /images/.test(lastCommand[2]));
   });
-});*/
+});
