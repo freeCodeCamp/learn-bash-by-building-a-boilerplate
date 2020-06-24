0d8169a72085c9b9b9f3b382a9b94e4b0795f50b
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 19:55:51 -0500

    470.1S

--- freeCodeCamp/test/L470.test.js
+++ freeCodeCamp/test/L470.test.js
@@ -13,3 +13,5 @@ describe('tests', () => {
     assert(websiteDirectory.indexOf('menlo.font') >= 0 && lastCommand[0] === 'touch' && /menlo\.font$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
