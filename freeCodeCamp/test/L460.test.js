46ae81a9f7f99e63384295032fa6a92d67c882b8
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 19:54:55 -0500

    460.1S

--- freeCodeCamp/test/L460.test.js
+++ freeCodeCamp/test/L460.test.js
@@ -13,3 +13,5 @@ describe('tests', () => {
     assert(websiteDirectory.indexOf('lato.font') >= 0 && lastCommand[0] === 'touch' && /lato\.font$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
