e5f8bc8dd825d7ce852c74ae2782bbc6f6ef7806
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 20:00:14 -0500

    510.1

--- freeCodeCamp/test/510.test.js
+++ freeCodeCamp/test/510.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -6,6 +6,6 @@ describe('tests', () => {
     const websiteDirectory = await getDirectoryContents('../website');
     const lastCommand = await getLastCommand();
 
-    assert(websiteDirectory.indexOf('CodeRoad.svg') >= 0 && lastCommand[0] === 'touch' && /CodeRoad\.svg$/.test(lastCommand[1]));
+    assert(websiteDirectory.indexOf('freeCodeCamp.svg') >= 0 && lastCommand[0] === 'touch' && /freeCodeCamp\.svg$/.test(lastCommand[1]));
   });
-});*/
+});
