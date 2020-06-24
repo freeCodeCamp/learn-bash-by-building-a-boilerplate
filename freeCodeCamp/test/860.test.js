7eab8b867373e4f1b85f2bfbb59adea3f788261b
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-24 10:33:44 -0500

    860.1

---
+++ freeCodeCamp/test/860.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getLastCommand, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  it('should pass', async () => {
+    const clientDirectory = await getDirectoryContents('../website/client');
+    const lastCommand = await getLastCommand();
+
+    assert(clientDirectory.indexOf('src') >= 0 && lastCommand[0] === 'mkdir' && /client\/src/.test(lastCommand[1]));
+  });
+});
