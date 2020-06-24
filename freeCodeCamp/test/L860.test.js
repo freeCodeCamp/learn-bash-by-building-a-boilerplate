558548a14ad6b02b41faee86418ac32903ff4745
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-24 10:33:44 -0500

    860.1

--- freeCodeCamp/test/L860.test.js
+++ freeCodeCamp/test/L860.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('tests', () => {
@@ -9,8 +9,6 @@ describe('tests', () => {
   });
 
   it('should pass', async () => {
-    assert(lastCommand[0] === 'mkdir' && lastCommand[1] === 'client' && cwd === '/home/strove/project/website');
+    assert(lastCommand[0] === 'mkdir' && lastCommand[1] === 'client/src' && cwd === '/home/strove/project/website');
   });
-});*/
-
-// solution
+});
