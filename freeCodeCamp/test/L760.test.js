6422ff8a8f6ee9c1796f8187c22ce28dd00e7896
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-24 10:15:40 -0500

    760.1S

--- freeCodeCamp/test/L760.test.js
+++ freeCodeCamp/test/L760.test.js
@@ -12,3 +12,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'mv' && lastCommand[1] === 'roboto.woff' && lastCommand[2] === 'fonts/roboto.woff' && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
