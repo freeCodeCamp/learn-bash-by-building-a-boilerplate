8a6a4f2e5a9debaa0a1e3ae6066e92511c35a8fa
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 22:03:03 -0500

    630.1

--- freeCodeCamp/test/L620.test.js
+++ freeCodeCamp/test/L620.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -12,6 +12,6 @@ describe('tests', () => {
   it('should pass', async () => {
     assert(imagesDirectory.indexOf('footer.jpeg') >= 0 && lastCommand[0] === 'cp' && /footer\.jpeg$/.test(lastCommand[1]) && /images/.test(lastCommand[2]) && cwd === '/home/strove/project/website');
   });
-});
+});*/
 
 // solution
