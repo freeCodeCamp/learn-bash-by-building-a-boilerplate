8945f104b1e0b2386124659d917e8d04a256144e
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-24 10:33:44 -0500

    860.1

---
+++ freeCodeCamp/test/L860.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, clientDirectory;
+  before(async () => {
+    clientDirectory = await getDirectoryContents('../website/client');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    assert(clientDirectory.indexOf('src') >= 0 && lastCommand[0] === 'mkdir' && /client\/src/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
+  });
+});
