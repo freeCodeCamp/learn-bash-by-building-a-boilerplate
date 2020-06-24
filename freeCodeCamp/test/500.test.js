bd5795f473104ec49fc397a6c7d490259b44cbf4
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 19:59:11 -0500

    500.1

--- freeCodeCamp/test/500.test.js
+++ freeCodeCamp/test/500.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -6,6 +6,6 @@ describe('tests', () => {
     const websiteDirectory = await getDirectoryContents('../website');
     const lastCommand = await getLastCommand();
 
-    assert(websiteDirectory.indexOf('CodeAlly.svg') >= 0 && lastCommand[0] === 'touch' && /CodeAlly\.svg$/.test(lastCommand[1]));
+    assert(websiteDirectory.indexOf('CodeRoad.svg') >= 0 && lastCommand[0] === 'touch' && /CodeRoad\.svg$/.test(lastCommand[1]));
   });
-});*/
+});
