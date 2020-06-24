063469379ace852e913373a0386756f64f5c60a1
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-24 10:38:25 -0500

    880.1

--- freeCodeCamp/reset.sh
+++ freeCodeCamp/reset.sh
@@ -7,13 +7,3 @@ fi
 
 find ./website -not -name '.' -not -name '..' -not -name 'website' -delete
 cp -r ./freeCodeCamp/reset_files/website/. ./website
-
-if [[ ! -a website/client ]]
-then 
-  mkdir website/client
-fi
-
-if [[ ! -a website/client/src ]]
-then 
-  mkdir website/client/src
-fi
