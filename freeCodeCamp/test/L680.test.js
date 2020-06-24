b4353f6c8de9a8a35da6160b6d3bc99b3a4a09b0
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 22:10:49 -0500

    680.1S

--- freeCodeCamp/test/L680.test.js
+++ freeCodeCamp/test/L680.test.js
@@ -13,3 +13,5 @@ describe('tests', () => {
     assert(websiteDirectory.indexOf('footer.jpeg') < 0 && lastCommand[0] === 'rm' && /footer\.jpeg$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
