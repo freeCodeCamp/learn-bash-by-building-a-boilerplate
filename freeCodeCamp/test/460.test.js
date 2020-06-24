ca7f5da1d64b6182599583b98c27b7efbbb8c236
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 19:54:47 -0500

    460.1

--- freeCodeCamp/test/460.test.js
+++ freeCodeCamp/test/460.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -6,6 +6,6 @@ describe('tests', () => {
     const websiteDirectory = await getDirectoryContents('../website');
     const lastCommand = await getLastCommand();
 
-    assert(websiteDirectory.indexOf('roboto.font') >= 0 && lastCommand[0] === 'touch' && /roboto\.font$/.test(lastCommand[1]));
+    assert(websiteDirectory.indexOf('lato.font') >= 0 && lastCommand[0] === 'touch' && /lato\.font$/.test(lastCommand[1]));
   });
-});*/
+});
