657e1ac5a09ef7d939973a49d9272b00072de365
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 22:13:13 -0500

    700.1

---
+++ freeCodeCamp/test/L700.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, websiteDirectory;
+  before(async () => {
+    websiteDirectory = await getDirectoryContents('../website');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    const filesAreCorrect = websiteDirectory.indexOf('roboto.font') < 0 && websiteDirectory.indexOf('roboto.woff') >= 0;
+    assert(filesAreCorrect && lastCommand[0] === 'mv' && /roboto\.font$/.test(lastCommand[1]) && /roboto\.woff$/.test(lastCommand[2]) && cwd === '/home/strove/project/website');
+  });
+});
