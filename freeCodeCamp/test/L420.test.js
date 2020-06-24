d1b45e4e20418bb7d41bb58159e070404160a111
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 19:50:27 -0500

    420.1

--- freeCodeCamp/test/L420.test.js
+++ freeCodeCamp/test/L420.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -10,8 +10,6 @@ describe('tests', () => {
   });
 
   it('should pass', async () => {
-    assert(websiteDirectory.indexOf('background.jpg') >= 0 && lastCommand[0] === 'touch' && /background\.jpg$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
+    assert(websiteDirectory.indexOf('header.png') >= 0 && lastCommand[0] === 'touch' && /header\.png$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
-});*/
-
-// solution
+});
