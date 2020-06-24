6d76e76ebb68dcd250efa89e6df9b2553899e716
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-24 10:18:05 -0500

    780.1

---
+++ freeCodeCamp/test/L780.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, websiteDirectory, fontsDirectory;
+  before(async () => {
+    websiteDirectory = await getDirectoryContents('../website');
+    fontsDirectory = await getDirectoryContents('../website/fonts');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    const filesAreCorrect = websiteDirectory.indexOf('lato.ttf') < 0 && fontsDirectory.indexOf('lato.ttf') >= 0;
+    assert(filesAreCorrect && lastCommand[0] === 'mv' && /lato\.ttf$/.test(lastCommand[1]) && /fonts/.test(lastCommand[2]) && cwd === '/home/strove/project/website');
+  });
+});
