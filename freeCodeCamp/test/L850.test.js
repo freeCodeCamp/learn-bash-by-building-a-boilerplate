3b3356813f2d78afe4ebfd35a9912660f74cb8c4
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-24 10:33:44 -0500

    860.1

--- freeCodeCamp/test/L850.test.js
+++ freeCodeCamp/test/L850.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -12,6 +12,6 @@ describe('tests', () => {
   it('should pass', async () => {
     assert(websiteDirectory.indexOf('client') >= 0 && lastCommand[0] === 'mkdir' && /client/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
-});
+});*/
 
 // solution
