c8c66652d9b730b39bf5cb6ef2246734b61e8ade
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-23 22:07:47 -0500

    660.1

--- freeCodeCamp/test/660.test.js
+++ freeCodeCamp/test/660.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('tests', () => {
@@ -6,6 +6,6 @@ describe('tests', () => {
     const lastCommand = await getLastCommand();
     const cwd = await getCwd();
 
-    assert(lastCommand[0] === 'cd' && /\/project\/website$/.test(cwd));
+    assert(lastCommand[0] === 'ls' && /\/project\/website$/.test(cwd));
   });
-});*/
+});
