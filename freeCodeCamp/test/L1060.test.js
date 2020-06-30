3f8f2f82d00fa73fe6e0133757d9d69a2a35028e
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-30 11:16:34 -0500

    1060.1

---
+++ freeCodeCamp/test/L1060.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, websiteDirectory, imagesDirectory;
+  before(async () => {
+    websiteDirectory = await getDirectoryContents('../website');
+    imagesDirectory = await getDirectoryContents('../website/images');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    const filesAreCorrect = websiteDirectory.indexOf('header.png') >= 0 && imagesDirectory.indexOf('header.png') < 0;
+    assert(filesAreCorrect && lastCommand[0] === 'mv' && /header\.png/.test(lastCommand[1]) && cwd === '/home/strove/project/website/images');
+  });
+});
