a74870877f0b0d6578b1bed8b8247b3dfb354fdd
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-30 11:13:27 -0500

    1040.1

--- freeCodeCamp/reset.sh
+++ freeCodeCamp/reset.sh
@@ -7,13 +7,3 @@ fi
 
 find ./website -not -name '.' -not -name '..' -not -name 'website' -delete
 cp -r ./freeCodeCamp/reset_files/website/. ./website
-
-if [[ ! -a website/client/assets ]]
-then 
-  mkdir website/client/assets
-fi
-
-if [[ ! -a website/client/assets/images ]]
-then 
-  mkdir website/client/assets/images
-fi
