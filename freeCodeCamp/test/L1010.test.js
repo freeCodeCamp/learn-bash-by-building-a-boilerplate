75308eb5c8eff8537ffc13ee29b9abc01d8919b9
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-30 11:11:12 -0500

    1020.1

--- freeCodeCamp/test/L1010.test.js
+++ freeCodeCamp/test/L1010.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -12,6 +12,6 @@ describe('tests', () => {
   it('should pass', async () => {
     assert(assetsDirectory.indexOf('images') >= 0 && lastCommand[0] === 'mkdir' && /images/.test(lastCommand[1]) && cwd === '/home/strove/project/website/client/assets');
   });
-});
+});*/
 
 // solution
