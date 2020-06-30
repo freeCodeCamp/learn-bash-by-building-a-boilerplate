9cec9c9398bb0155ec67a15c187933b84215e2da
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-30 11:13:27 -0500

    1040.1

--- freeCodeCamp/test/1030.test.js
+++ freeCodeCamp/test/1030.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -8,4 +8,4 @@ describe('tests', () => {
 
     assert(imagesDirectory.indexOf('background.jpg') >= 0 && lastCommand[0] === 'touch' && /background\.jpg$/.test(lastCommand[1]));
   });
-});
+});*/
