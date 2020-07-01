7e03ae1cbc3d40d67079a9663d4cc390d2a04648
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:32:10 -0500

    1270.1S

--- freeCodeCamp/test/L1270.test.js
+++ freeCodeCamp/test/L1270.test.js
@@ -12,3 +12,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'touch' && lastCommand[1] === 'roboto-light.woff' && cwd === '/home/strove/project/website/client/assets/fonts');
   });
 });
+
+// solution
