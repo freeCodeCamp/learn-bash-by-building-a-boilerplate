d21675acbad704afc80060eb47c7667d08bf2ec9
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:32:04 -0500

    1270.1

--- freeCodeCamp/test/1270.test.js
+++ freeCodeCamp/test/1270.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -6,6 +6,6 @@ describe('tests', () => {
     const fontsDirectory = await getDirectoryContents('../website/client/assets/fonts');
     const lastCommand = await getLastCommand();
 
-    assert(fontsDirectory.indexOf('roboto-bold.woff') >= 0 && lastCommand[0] === 'touch' && /client\/assets\/fonts\/roboto-bold\.woff/.test(lastCommand[1]));
+    assert(fontsDirectory.indexOf('roboto-light.woff') >= 0 && lastCommand[0] === 'touch' && /client\/assets\/fonts\/roboto-light\.woff$/.test(lastCommand[1]));
   });
-});*/
+});
