bc5265041973636d5c7395ed43e99033894152bc
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:24:00 -0500

    1210.1

--- freeCodeCamp/test/1200.test.js
+++ freeCodeCamp/test/1200.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -8,4 +8,4 @@ describe('tests', () => {
 
     assert(assetsDirectory.indexOf('icons') >= 0 && lastCommand[0] === 'mkdir' && /client\/assets\/icons/.test(lastCommand[1]));
   });
-});
+});*/
