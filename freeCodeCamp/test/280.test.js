db151950d015685273add6d6c9956cf379589568
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 14:30:09 -0500

    285.1

--- freeCodeCamp/test/280.test.js
+++ freeCodeCamp/test/280.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -8,4 +8,4 @@ describe('tests', () => {
 
     assert(projectDirectory.indexOf('website') >= 0 && lastCommand[0] === 'mkdir' && /website/g.test(lastCommand[1]));
   });
-});
+});*/
