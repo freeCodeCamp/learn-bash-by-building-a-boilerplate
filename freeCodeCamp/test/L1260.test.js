b11487afe9f4af4fdbd298814e57d4d095e73938
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:30:49 -0500

    1260.1

---
+++ freeCodeCamp/test/L1260.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, fontsDirectory;
+  before(async () => {
+    fontsDirectory = await getDirectoryContents('../website/client/assets/fonts');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    assert(fontsDirectory.indexOf('roboto-bold.woff') >= 0 && lastCommand[0] === 'touch' && /client\/assets\/fonts\/roboto-bold\.woff/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
+  });
+});
