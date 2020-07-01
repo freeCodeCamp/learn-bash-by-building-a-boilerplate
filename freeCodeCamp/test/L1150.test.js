41af22256c7865863491ae0ae6ae850d4b83386d
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 09:01:23 -0500

    1150.1

---
+++ freeCodeCamp/test/L1150.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    assert(lastCommand[0] === 'rmdir' && /images/.test(lastCommand[1]) && !/assets/.test(lastCommand[1]) && lastCommand[2] === undefined && cwd === '/home/strove/project/website');
+  });
+});
