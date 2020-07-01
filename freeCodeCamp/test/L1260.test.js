0aaec6e9008bdc6bda5de33a8c7f5db2fea65927
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:31:01 -0500

    1260.1S

--- freeCodeCamp/test/L1260.test.js
+++ freeCodeCamp/test/L1260.test.js
@@ -12,3 +12,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'touch' && lastCommand[1] === 'roboto-bold.woff' && cwd === '/home/strove/project/website/client/assets/fonts');
   });
 });
+
+// solution
