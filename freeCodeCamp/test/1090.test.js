c9e713e2b07eff4961166df6ec6c095d89105359
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 08:51:10 -0500

    1100.1

--- freeCodeCamp/test/1090.test.js
+++ freeCodeCamp/test/1090.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('tests', () => {
@@ -8,4 +8,4 @@ describe('tests', () => {
 
     assert(lastCommand[0] === 'find' && lastCommand[1] === '-name' && lastCommand[2] === 'images' && /\/project\/website$/.test(cwd));
   });
-});
+});*/
