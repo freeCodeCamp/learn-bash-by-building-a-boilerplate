3c8d85803b1156ce2f2b2d6ad4e8a6b44375f76c
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:34:01 -0500

    1280.1

--- freeCodeCamp/test/L1280.test.js
+++ freeCodeCamp/test/L1280.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('tests', () => {
@@ -9,8 +9,6 @@ describe('tests', () => {
   });
 
   it('should pass', async () => {
-    assert(lastCommand[0] === 'touch' && lastCommand[1] === 'roboto-light.woff' && cwd === '/home/strove/project/website/client/assets/fonts');
+    assert(lastCommand[0] === 'touch' && lastCommand[1] === 'lato-bold.ttf' && cwd === '/home/strove/project/website/client/assets/fonts');
   });
-});*/
-
-// solution
+});
