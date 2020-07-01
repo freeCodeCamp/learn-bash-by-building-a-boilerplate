4ebb6671aecef9c1eb415c2c1eeab2f1f6366237
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 08:49:46 -0500

    1090.1S

--- freeCodeCamp/test/L1090.test.js
+++ freeCodeCamp/test/L1090.test.js
@@ -12,3 +12,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'find' && lastCommand[1] === '-name' && lastCommand[2] === 'images' && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
