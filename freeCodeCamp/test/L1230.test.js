61030bd999a02c603259c6a8caa174842367077c
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:27:08 -0500

    1230.1S

--- freeCodeCamp/test/L1230.test.js
+++ freeCodeCamp/test/L1230.test.js
@@ -12,3 +12,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'mv' && lastCommand[1] === 'icons' && lastCommand[2] === 'client/assets' && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
