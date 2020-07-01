9b54d5421d5fb37e02987732c721e6416fec238b
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 08:58:29 -0500

    1130.1

---
+++ freeCodeCamp/test/L1130.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, imagesDirectory1, imagesDirectory2;
+  before(async () => {
+    imagesDirectory1 = await getDirectoryContents('../website/images');
+    imagesDirectory2 = await getDirectoryContents('../website/client/assets/images');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    const filesAreCorrect = imagesDirectory1.indexOf('footer.jpeg') < 0 && imagesDirectory2.indexOf('footer.jpeg') >= 0;
+    assert(filesAreCorrect && lastCommand[0] === 'mv' && /images\/footer\.jpeg$/.test(lastCommand[1]) && /client\/assets\/images/.test(lastCommand[2]) && cwd === '/home/strove/project/website');
+  });
+});
