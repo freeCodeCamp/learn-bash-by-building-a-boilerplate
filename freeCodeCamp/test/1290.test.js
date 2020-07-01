b45168929c18a5f46899552a347de806a31a7371
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 10:35:14 -0500

    1290.1

--- freeCodeCamp/test/1290.test.js
+++ freeCodeCamp/test/1290.test.js
@@ -1,11 +1,11 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
-  it('should pass', async () => {
     const fontsDirectory = await getDirectoryContents('../website/client/assets/fonts');
     const lastCommand = await getLastCommand();
 
-    assert(fontsDirectory.indexOf('lato-bold.ttf') >= 0 && lastCommand[0] === 'touch' && /client\/assets\/fonts\/lato-bold\.ttf$/.test(lastCommand[1]));
+  it('should pass', async () => {
+    assert(fontsDirectory.indexOf('lato-light.ttf') >= 0 && lastCommand[0] === 'touch' && /client\/assets\/fonts\/lato-light\.ttf$/.test(lastCommand[1]));
   });
-});*/
+});
