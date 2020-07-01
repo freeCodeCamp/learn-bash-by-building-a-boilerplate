8ed398eab81039feebd887d2c2ec451cf18ad4d8
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-01 08:51:21 -0500

    1100.1S

--- freeCodeCamp/test/L1100.test.js
+++ freeCodeCamp/test/L1100.test.js
@@ -12,3 +12,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'mv' && lastCommand[1] === 'header.png' && lastCommand[2] === 'client/assets/images' && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
