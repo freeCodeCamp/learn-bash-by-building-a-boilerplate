b7cc3fb060dbc692370e680cdca45c4d8d36215e
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 17:40:44 -0500

    1360.1

--- freeCodeCamp/test/1350.test.js
+++ freeCodeCamp/test/1350.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -8,4 +8,4 @@ describe('tests', () => {
 
     assert(websiteDirectory.indexOf('README.md') >= 0 && lastCommand[0] === 'touch' && /README\.md$/.test(lastCommand[1]));
   });
-});
+});*/
