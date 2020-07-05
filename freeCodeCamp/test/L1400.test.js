0b19afaccb206eae6154fa803bd2be22cf7396f4
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 17:54:36 -0500

    1400.1S

--- freeCodeCamp/test/L1400.test.js
+++ freeCodeCamp/test/L1400.test.js
@@ -11,3 +11,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'echo' && lastCommand[1] === 'I' && lastCommand[2] === 'made' && lastCommand[3] === 'this' && lastCommand[4] === 'boilerplate' && lastCommand[5].op === `>>` && /README\.md$/.test(lastCommand[6]));
   });
 });
+
+// solution
