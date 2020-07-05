baa62beea04c9ae4e9edde0d89d42b41a09c3a10
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 18:13:41 -0500

    1450.1

---
+++ freeCodeCamp/test/L1450.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, projectDirectory;
+  before(async () => {
+    projectDirectory = await getDirectoryContents('..');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    const filesAreCorrect = projectDirectory.indexOf('website') < 0 && projectDirectory.indexOf('website-boilerplate') >= 0;
+    assert(filesAreCorrect && lastCommand[0] === 'mv' && /website/.test(lastCommand[1]) && /website-boilerplate/.test(lastCommand[2]) && cwd === '/home/strove/project');
+  });
+});
