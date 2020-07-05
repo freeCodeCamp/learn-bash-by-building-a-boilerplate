ac20c90a69adac3ac5955b08f271bb735d0bf37c
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 17:38:35 -0500

    1340.1S

--- freeCodeCamp/test/L1340.test.js
+++ freeCodeCamp/test/L1340.test.js
@@ -13,3 +13,5 @@ describe('tests', () => {
     assert(websiteDirectory.indexOf('server.js') >= 0 && lastCommand[0] === 'touch' && /server\.js$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
