4762c160b230d637996186c3c1b746f68cd88270
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 10:21:54 -0500

    1315.1

---
+++ freeCodeCamp/test/L1315.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, websiteDirectory;
+  before(async () => {
+    websiteDirectory = await getDirectoryContents('../website');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    const flags = ['-r', '-R', '--recursive'];
+    const correctArguments1 = /fonts/.test(lastCommand[1]) && flags.indexOf(lastCommand[2]) >= 0;
+    const correctArguments2 = /fonts/.test(lastCommand[2]) && flags.indexOf(lastCommand[1]) >= 0;
+    const correctArgs = correctArguments1 || correctArguments2;
+    assert(websiteDirectory.indexOf('fonts') < 0 && lastCommand[0] === 'rm' && correctArgs && cwd === '/home/strove/project/website');
+  });
+});
