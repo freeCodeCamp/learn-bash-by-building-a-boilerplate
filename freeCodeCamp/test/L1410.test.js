654f86ab0bb1e9fc6dbf5c44732ead51c8b8d527
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 18:03:38 -0500

    1410.1S

--- freeCodeCamp/test/L1410.test.js
+++ freeCodeCamp/test/L1410.test.js
@@ -11,3 +11,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'echo' && lastCommand[1] === 'from' && lastCommand[2] === 'the' && lastCommand[3] === 'command' && lastCommand[4] === 'line' && lastCommand[5].op === `>>` && /README\.md$/.test(lastCommand[6]));
   });
 });
+
+// solution
