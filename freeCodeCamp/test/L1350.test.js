666f3a5394fef198adc92f2096f5979a81329f88
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 17:39:34 -0500

    1350.1S

--- freeCodeCamp/test/L1350.test.js
+++ freeCodeCamp/test/L1350.test.js
@@ -13,3 +13,5 @@ describe('tests', () => {
     assert(websiteDirectory.indexOf('README.md') >= 0 && lastCommand[0] === 'touch' && /README\.md$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
 });
+
+// solution
