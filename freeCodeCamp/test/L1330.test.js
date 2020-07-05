fae1aab0d2a3ce041226a5af4ebbcded184a4770
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 17:36:57 -0500

    1330.1

---
+++ freeCodeCamp/test/L1330.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, websiteDirectory;
+  before(async () => {
+    websiteDirectory = await getDirectoryContents('../website');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    assert(websiteDirectory.indexOf('package.json') >= 0 && lastCommand[0] === 'touch' && /package\.json$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
+  });
+});
