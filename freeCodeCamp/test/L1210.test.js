763f74b36f68ed26138acb255931882d1effc4a0
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 08:54:55 -0500

    1215.1

--- freeCodeCamp/test/L1210.test.js
+++ freeCodeCamp/test/L1210.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -14,6 +14,6 @@ describe('tests', () => {
     const filesAreCorrect = websiteDirectory.indexOf('Strove.svg') < 0 && iconsDirectory.indexOf('Strove.svg') >= 0;
     assert(filesAreCorrect && lastCommand[0] === 'mv' && /Strove\.svg$/.test(lastCommand[1]) && /client\/assets\/icons/.test(lastCommand[2]) && cwd === '/home/strove/project/website');
   });
-});
+});*/
 
 // solution
