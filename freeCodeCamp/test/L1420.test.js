acec4caf750f165d24fe330e17907357fc00a700
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 18:05:00 -0500

    1420.1S

--- freeCodeCamp/test/L1420.test.js
+++ freeCodeCamp/test/L1420.test.js
@@ -11,3 +11,5 @@ describe('tests', () => {
     assert(lastCommand[0] === 'echo' && lastCommand[1] === 'for' && lastCommand[2] === `the` && lastCommand[3] === 'freeCodeCamp' && lastCommand[4] === 'bash' && lastCommand[5] === 'lessons' && lastCommand[6].op === `>>` && /README\.md$/.test(lastCommand[7]));
   });
 });
+
+// solution
