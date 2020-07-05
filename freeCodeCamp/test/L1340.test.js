85f4804468a4b4633838d488ff54ccfd88fd4858
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 17:38:25 -0500

    1340.1

--- freeCodeCamp/test/L1340.test.js
+++ freeCodeCamp/test/L1340.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
 
 describe('tests', () => {
@@ -10,8 +10,6 @@ describe('tests', () => {
   });
 
   it('should pass', async () => {
-    assert(websiteDirectory.indexOf('package.json') >= 0 && lastCommand[0] === 'touch' && /package\.json$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
+    assert(websiteDirectory.indexOf('server.js') >= 0 && lastCommand[0] === 'touch' && /server\.js$/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
   });
-});*/
-
-// solution
+});
