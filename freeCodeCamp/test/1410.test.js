9002eaa995ba327b7ab742d2e52b5fb455584f6a
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 20:20:34 -0500

    1415.1

--- freeCodeCamp/test/1410.test.js
+++ freeCodeCamp/test/1410.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('tests', () => {
@@ -7,4 +7,4 @@ describe('tests', () => {
 
     assert(lastCommand[0] === 'echo' && lastCommand[1] === 'from' && lastCommand[2] === 'the' && lastCommand[3] === 'command' && lastCommand[4] === 'line' && lastCommand[5].op === `>>` && /README\.md$/.test(lastCommand[6]));
   });
-});
+});*/
