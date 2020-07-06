44b85f36e0edb077dbfce5cb8c93223bc6affcd5
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 20:19:10 -0500

    1405.1

--- freeCodeCamp/test/L1400.test.js
+++ freeCodeCamp/test/L1400.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('tests', () => {
@@ -10,6 +10,6 @@ describe('tests', () => {
   it('should pass', async () => {
     assert(lastCommand[0] === 'echo' && lastCommand[1] === 'I' && lastCommand[2] === 'made' && lastCommand[3] === 'this' && lastCommand[4] === 'boilerplate' && lastCommand[5].op === `>>` && /README\.md$/.test(lastCommand[6]));
   });
-});
+});*/
 
 // solution
