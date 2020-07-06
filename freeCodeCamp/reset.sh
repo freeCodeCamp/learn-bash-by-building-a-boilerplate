aae8f64a38c5fc9ad2c2a5af05403a6613cca5a4
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-05 20:32:27 -0500

    1475.1

--- freeCodeCamp/reset.sh
+++ freeCodeCamp/reset.sh
@@ -10,5 +10,13 @@ then
   mkdir website-boilerplate
 fi
 
+if [[ ! -a toms-website ]]
+then 
+  mkdir toms-website
+fi
+
 find ./website-boilerplate -not -name '.' -not -name '..' -not -name 'website-boilerplate' -delete
 cp -r ./freeCodeCamp/reset_files/website-boilerplate/. ./website-boilerplate
+
+find ./toms-website -not -name '.' -not -name '..' -not -name 'toms-website' -delete
+cp -r ./freeCodeCamp/reset_files/toms-website/. ./toms-website
