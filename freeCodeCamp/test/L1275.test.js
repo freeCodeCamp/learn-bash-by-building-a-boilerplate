5d009ff3d01c0496c8e62a1d806079580f3a8e2e
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-06 17:27:33 -0500

    1275.1

---
+++ freeCodeCamp/test/L1275.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    assert(lastCommand[0] === 'find' && /client\/assets\/fonts/.test(lastCommand[1]) && cwd === '/home/strove/project/website');
+  });
+});
